2c8924cd2ffbb438961ec4f4d9256d262ba98df5
RogueM <chaosgen6@googlemail.com>
2016-10-01 12:14:38 +0100

    separate probe for busses

--- library/envirophat/__init__.py
+++ library/envirophat/__init__.py
@@ -5,16 +5,20 @@ from .leds import leds
 from .lsm303d import lsm303d
 from .tcs3472 import tcs3472
 
+leds = leds()
+
+try:
+    light = tcs3472(bus)
+    weather = bmp280(bus)
+    analog = ads1015(bus)
+    motion = lsm303d(bus)
+except IOError:
+    print "can't find Enviro pHAT on the i2c1 bus"
+
 try:
     altlight = tcs3472(altbus)
     altweather = bmp280(altbus)
     altanalog = ads1015(altbus)
     altmotion = lsm303d(altbus)
 except IOError:
-    print "using main bus"    
-else:
-    leds = leds()
-    light = tcs3472(bus)
-    weather = bmp280(bus)
-    analog = ads1015(bus)
-    motion = lsm303d(bus)
+    pass
